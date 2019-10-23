ba06363a3dceaa7bbeb7ce9cc25e50cce353ac02
ThowV <thomasvisscher@outlook.com>
2019-10-23 14:54:46 +0200

    Added ability to read light intensity

--- Unit/src/sensors.c
+++ Unit/src/sensors.c
@@ -3,12 +3,22 @@
 
 float getTemperature() {
 	// Get reading from temperature sensor, ADC PIN 0
-	int temperatureInput = analogRead(0);
+	int temperatureSensorInput = analogRead(0);
 	
-	float temperature = (float)temperatureInput / 1024;	// Find percentage of input reading: ranging from 0 to 1023
-	temperature = temperature * 5;						// Multiply by 5V to get voltage
-	temperature = temperature - 0.5;					// Subtract the offset which is 0.5V
-	temperature = temperature * 100;					// Convert to degrees
+	float temperature = (float)temperatureSensorInput / 1024;	// Find percentage of input reading: ranging from 0 to 1023
+	
+	temperature = temperature * 5;		// Multiply by 5V to get voltage
+	temperature = temperature - 0.5;	// Subtract the offset which is 0.5V
+	temperature = temperature * 100;	// Convert to degrees
 	
 	return temperature;
+}
+
+float getLightIntensity() {
+	// Get reading from light sensor, ADC PIN 1
+	int lightSensorInput = analogRead(1);
+	
+	float lightIntensity = (float)lightSensorInput / 10;	// Divide intensity value by 10 to stay inside 127 range of int8
+	
+	return lightIntensity;
 }
