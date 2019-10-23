90736e28c414678a73e69f9bf2aec11a4649d430
ThowV <thomasvisscher@outlook.com>
2019-10-23 13:57:55 +0200

    Added temperature reading to main.c

--- Unit/src/main.c
+++ Unit/src/main.c
@@ -4,6 +4,7 @@
 #include <util/delay.h>
 
 #include "UART.h"
+#include "sensors.h"
 
 #define BLINK_DELAY_MS 1000
 
@@ -24,23 +25,16 @@ void setup(void) {
 	_delay_ms(1000);
 }
 
-void loop(void) {
-	/* set pin 5 high to turn led on */
-	PORTB |= _BV(PORTB5);
-	_delay_ms(BLINK_DELAY_MS);
-
-	/* set pin 5 low to turn led off */
-	PORTB &= ~_BV(PORTB5);
-	_delay_ms(BLINK_DELAY_MS);
+void loop() {
+	int8_t temperature = (int8_t)getTemperature();
+	
+	transmitData(temperature);_delay_ms(1000);
 }
 
 int main (void)
 {
 	setup();
 	
-	/* set pin 5 of PORTB for output*/
-	DDRB |= _BV(DDB5);
-
 	while(1) {
 		loop();
 	}
