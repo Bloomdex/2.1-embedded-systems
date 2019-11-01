f63b9a2a831d7873493e91f0e9e98d8b793ab2ea
LutzenH <46445220+LutzenH@users.noreply.github.com>
2019-11-01 16:13:22 +0100

    Added comments and ISR for receiving data

--- Unit/src/main.c
+++ Unit/src/main.c
@@ -1,4 +1,5 @@
 #include <avr/io.h>
+#include <avr/interrupt.h>
 
 #define F_CPU 16E6	// Frequency definition for delay.h
 #include <util/delay.h>
@@ -15,6 +16,9 @@ void setup(void) {
 	initUART();
 	initPortManipulator();
 	
+	// Enables interrupts by setting the global interrupt mask
+	sei();
+
 	_delay_ms(1000);
 }
 
@@ -42,4 +46,17 @@ int main (void)
 	}
 	
 	return 0;
+}
+
+//Interrupt Service Routine for when data is received.
+ISR(USART_RX_vect)
+{
+	//Checks if any data has been sent.
+	if (UCSR0A & (1<<RXC0)) {
+		unsigned char data = UDR0;
+
+		//Stores the data in the received_data queue, so it can handle the received data at a later moment.
+    	received_data[received_data_index] = data;
+    	received_data_index = (received_data_index + 1) % RECEIVED_DATA_SIZE;
+	}
 }
--- Unit/src/serial.c
+++ Unit/src/serial.c
@@ -3,27 +3,36 @@
 #include "UART.h"
 #include "serial.h"
 
+// Queue for received data.
+volatile unsigned char received_data[RECEIVED_DATA_SIZE];
+volatile uint8_t received_data_index = 0;
+
+// Buffer for measured temperatures.
 int8_t temperatures[TEMPERATURE_STORAGE_SIZE];
-int8_t temperature_head_index = 0;
+uint8_t temperature_head_index = 0;
 
+// Buffer for measured light intensity.
 int8_t lights[LIGHT_STORAGE_SIZE];
-int8_t lights_head_index = 0;
+uint8_t lights_head_index = 0;
 
+//Adds the given temperature value to the temperature buffer.
 void addTemperatureToBuffer(int8_t value) {
     temperatures[temperature_head_index] = value;
     temperature_head_index = (temperature_head_index + 1) % TEMPERATURE_STORAGE_SIZE;
 }
 
+//Adds the given light value to the light buffer.
 void addLightToBuffer(int8_t value) {
     lights[lights_head_index] = value;
     lights_head_index = (lights_head_index + 1) % LIGHT_STORAGE_SIZE;
 }
 
+// Transmits data that has been collected in the buffer. (this will not reset the buffer)
 void transmitBufferData(char buffercode, int8_t storagebuffer[], unsigned char storageSize, int8_t bufferIndex) {
     transmitData(buffercode);
 
-    for (int i = 0; i < storageSize; i++) {
-        int index = (i + bufferIndex) % storageSize;
+    for (uint8_t i = 0; i < storageSize; i++) {
+        uint8_t index = (i + bufferIndex) % storageSize;
         
         if(storagebuffer[index] != 0x00)
             transmitData(storagebuffer[index]);
@@ -32,18 +41,32 @@ void transmitBufferData(char buffercode, int8_t storagebuffer[], unsigned char s
     transmitData(0x00);
     transmitData(buffercode);
     transmitData(0x00);
-
-    memset(storagebuffer, 0, TEMPERATURE_STORAGE_SIZE);
 }
 
+// Handles possible instruction in the received_data queue.
 void handleInstructions(void) {
-    uint8_t receivedData = receiveData();
-    switch(receivedData) {
-        case TEMPERATURE_CODE:            
-            transmitBufferData(TEMPERATURE_CODE, temperatures, TEMPERATURE_STORAGE_SIZE, temperature_head_index);
-            break;
-        case LIGHT_CODE:
-            transmitBufferData(LIGHT_CODE, lights, LIGHT_STORAGE_SIZE, lights_head_index);
-            break;
+    //Loops over every data point in received_data
+    for(uint8_t i = 0; i < RECEIVED_DATA_SIZE; i++) {
+        uint8_t index = (i + received_data_index) % RECEIVED_DATA_SIZE;
+
+        // Checks if the current data in received_data is an instruction.
+        switch(received_data[index]) {
+            case TEMPERATURE_CODE:            
+                transmitBufferData(TEMPERATURE_CODE, temperatures, TEMPERATURE_STORAGE_SIZE, temperature_head_index);
+                memset(temperatures, 0, TEMPERATURE_STORAGE_SIZE);
+                temperature_head_index = 0;
+                break;
+            case LIGHT_CODE:
+                transmitBufferData(LIGHT_CODE, lights, LIGHT_STORAGE_SIZE, lights_head_index);
+                memset(lights, 0, LIGHT_STORAGE_SIZE);
+                lights_head_index = 0;
+                break;
+        }
+
+        // Resets the index of the previous value to 0. 
+        received_data[index] = 0;
     }
+
+    // Resets the index of the queue back to the front of queue
+    received_data_index = 0;
 }
