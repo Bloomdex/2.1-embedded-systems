0624c075d23ffddb0f937a06d683eace5b1dff5a
ThowV <thomasvisscher@outlook.com>
2019-11-07 22:34:54 +0100

    Updated header file for rollerShutter.c

--- Unit/src/Header Files/rollerShutter.h
+++ Unit/src/Header Files/rollerShutter.h
@@ -4,6 +4,12 @@
 void setRollerShutterClosed();
 void setRollerShutterOpened();
 void setRollerShutterAnimating();
+
+void setShutterForceClosed();
+void setShutterForceOpened();
+void setShutterFreed();
+uint8_t getRollerShutterState();
+
 void rollerShutterUpdate(int8_t temperature, int8_t lightIntensity, int8_t prefferedTemperature, int8_t prefferedLightIntensity);
 
 #endif /* ROLLERSHUTTER_H_ */
